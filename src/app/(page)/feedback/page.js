59b6664f457b24d94780f4a7040875f71d47d6a6
keyur <keyurpansuriya88@outlook.com>
2024-03-18 13:38:14 +0530

    E-commerce Remove Page And Componets

--- src/app/(page)/feedback/page.js
+++
@@ -1,8 +0,0 @@
-export default function Feedback()
-{
-    return(
-        <div className="text-center">
-            <img src="feedback-4746811_640.webp" />
-        </div>
-    )
-}
--- src/app/componets/Buttons/show_product_btn.js
+++
@@ -1,6 +0,0 @@
-export default function Show_product_btn()
-{
-  return(
-    <button className="btn btn-secondary" >Show</button>
-  )
-}
